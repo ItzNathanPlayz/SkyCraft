549dd53bd2a08cd79409fd51416880bbef9245e7
ItzNathanPlayz <36180480+ItzNathanPlayz@users.noreply.github.com>
2018-02-05 21:52:45 -0600

    Update SkyCraft.js

--- SkyCraft.js
+++ SkyCraft.js
@@ -1,11 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const bot = new Discord.Client({disableEveryone: true})
 
 client.on('ready', () => {
-    console.log('Starting..');
+    console.log('I am ready!');
 });
-bot.user.setGame("OriginPvP | /help");
 
 client.on('message', message => {
     if (message.content === 'ping') {
